263419c48f973affc5ece497c9a2dd927a24a2ae
Jonni Åkesson <devbyjonni@gmail.com>
2025-02-09 18:51:15 +0100

    Added authentication route /token

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/")
 def read_root():
     return {"message": "MFF Players API is running!"}
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    if form_data.username != "testuser" or form_data.password != "password123":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
+    return {"access_token": form_data.username, "token_type": "bearer"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
